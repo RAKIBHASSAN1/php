44a4cca3e52d0634961e648099da36456f3a1b6c
RAKIBHASSAN1 <47098282+RAKIBHASSAN1@users.noreply.github.com>
2019-04-18 14:28:29 +0600

    Add files via upload

---
+++ scripts/calculator.js
@@ -0,0 +1,72 @@
+ function calc()
+            {
+                var n1 = parseFloat(document.getElementById('n1').value);
+                var oper = document.getElementById('operators').value;
+                
+                if(oper === 'Afghan Afghani')
+                {
+                    
+                    document.getElementById('result').value = n1*0.92;
+                }
+                
+                if(oper === 'Albanian Lek')
+                {
+                    document.getElementById('result').value = n1*1.33;
+                }
+                
+                if(oper === 'Algerian Dinar')
+                {
+                    document.getElementById('result').value = n1*1.42;
+                }
+                
+                if(oper === 'Angolan Kwanza')
+                {
+                    document.getElementById('result').value = n1*3.78;
+                }
+                if(oper === 'Argentine Peso')
+                {
+                    document.getElementById('result').value = n1*0.52;
+                }
+                if(oper === 'Armenian Dram')
+                {
+                    document.getElementById('result').value = n1*5.81;
+                }
+                if(oper === 'Aruban Florin')
+                {
+                    document.getElementById('result').value = n1*0.021;
+                }
+                if(oper === 'Australian Dollar')
+                {
+                    document.getElementById('result').value = n1*0.017;
+                }
+                if(oper === 'Azerbaijani Manat')
+                {
+                    document.getElementById('result').value = n1*0.020;
+                }
+                if(oper === 'Bahamian Dollar')
+                {
+                    document.getElementById('result').value = n1*0.012;
+                }
+                if(oper === 'Bahraini Dinar')
+                {
+                    document.getElementById('result').value = n1*0.0045;
+                }
+                if(oper === 'Bajan dollar')
+                {
+                    document.getElementById('result').value = n1*0.024;
+                }
+                if(oper === 'Belarusian Ruble')
+                {
+                    document.getElementById('result').value = n1*0.025;
+                }
+                if(oper === 'Belize Dollar')
+                {
+                    document.getElementById('result').value = n1*0.024;
+                }
+                if(oper === 'Bitcoin')
+                {
+                    document.getElementById('result').value = n1*0.0000023;
+                }
+               
+                
+            }
